db07ffcaf664d0d340f585977dafa5bfa123d545
David oga <104001201+OgaDavid@users.noreply.github.com>
2023-04-24 20:48:14 -1100

    Update Profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,7 +4,7 @@ const Profile = () => {
   return (
     <section className="profile">
       <img src="images/profile_pic.JPG" alt="" className="profile__img"  id='profile__img'/>
-      <p className='profile__name' id='twitter'>Demiicoder</p>
+      <p className='profile__name' id='twitter'>Demicoderr</p>
       <p className="slack" id='slack'>David Oga</p>
     </section>
   )
